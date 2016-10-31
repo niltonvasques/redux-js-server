95ba44fddd2544fff38e4fb72ef3ddec89b36bbb
Nilton Vasques <nilton.vasques@gmail.com>
2016-10-31 04:59:26 -0300

    Add a log to know about state changes

--- src/server.js
+++ src/server.js
@@ -4,7 +4,10 @@ export function startServer(store) {
   const io = new Server().attach(8090);
 
   store.subscribe(
-    () => io.emit('state', store.getState().toJS())
+    () => {
+      console.log(store.getState());
+      io.emit('state', store.getState().toJS());
+    }
   );
 
   io.on('connection', (socket) => {
